cab8116eddb4ea66c41708d6b3122daad2d26ada
HusseinHany <hhany3997@gmail.com>
2023-10-03 08:57:37 +0300

    Define category model

---
+++ models/category.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+      trim : true
+  },
+    description: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    }
+})
+
+// URL virtual for category
+categorySchema
+    .virtual('url')
+    .get(function () {
+        return '/category/' + this._id;
+    }
+);
+
+// Export model
+module.exports = mongoose.model('Category', categorySchema);
